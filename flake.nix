cb420ca34287da628c54d07918574c80ee107d31
bandithedoge <bandithedoge@protonmail.com>
2022-02-11 22:57:22 +0100

    add ts parsers

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    norg.url = "github:nvim-neorg/tree-sitter-norg";
+    norg-meta.url = "github:nvim-neorg/tree-sitter-norg-meta";
+    norg-table.url = "github:nvim-neorg/tree-sitter-norg-table";
+
     neorg = {
       url = "github:nvim-neorg/neorg";
       flake = false;
@@ -11,9 +15,16 @@
       flake = false;
     };
   };
-  outputs = { nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     overlay = final: prev:
-      with inputs; {
+      with inputs; let
+        grammars = {
+          tree-sitter-norg = norg.defaultPackage;
+          tree-sitter-norg-meta = norg-meta.defaultPackage;
+          tree-sitter-norg-table = norg-table.defaultPackage;
+        };
+      in
+      {
         vimPlugins = prev.vimPlugins // {
           neorg = prev.vimUtils.buildVimPluginFrom2Nix {
             name = "neorg";
@@ -26,6 +37,11 @@
             buildInputs = [ prev.vimPlugins.telescope-nvim final.vimPlugins.neorg ];
           };
         };
+        tree-sitter = prev.tree-sitter // {
+          allGrammars = (prev.lib.lists.remove prev.tree-sitter.builtGrammars.tree-sitter-norg prev.tree-sitter.allGrammars) ++ builtins.attrValues grammars;
+          builtGrammars = prev.tree-sitter.builtGrammars // grammars;
+        };
+        tree-sitter-grammars = prev.tree-sitter-grammars // grammars;
       };
   };
 }
