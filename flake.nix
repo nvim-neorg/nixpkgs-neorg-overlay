617be4f6137eed28dcb912a52f644c03c4e293bf
Paul Schyska <pschyska@gmail.com>
2022-11-18 14:42:43 +0100

    Use vimPlugin.extend, try to be more clever about treesitter grammars, add tests
    vimPlugin.extend should prevent inter-plugin dependency breakage.
    vimPlugins.nvim-treesitter.{withPlugins,withAllGrammars} now auto-adds the required norg
    grammars. I didn't find a way to make plain vimPlugins.nvim-treesitter work without
    triggering inifinite recursion.
    
    See the tests for for detailed behaviour. I also added a GitHub workflow that runs them.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    flake-utils.url = "github:numtide/flake-utils";
+
     norg.url = "github:nvim-neorg/tree-sitter-norg";
     norg-meta.url = "github:nvim-neorg/tree-sitter-norg-meta";
 
@@ -14,34 +16,60 @@
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, ... }@inputs: {
-    overlay = final: prev:
-      with inputs; let
-        grammars = {
-          tree-sitter-norg = norg.defaultPackage.${final.system};
-          tree-sitter-norg-meta = norg-meta.defaultPackage.${final.system};
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , norg
+    , norg-meta
+    , neorg
+    , neorg-telescope
+    , ...
+    }: {
+      overlays.default = final: prev:
+        let inherit (final.lib) attrValues elem filter filterAttrs isDerivation; in
+        {
+          vimPlugins = prev.vimPlugins.extend (f: p: {
+            nvim-treesitter =
+              let
+                norgGrammars = {
+                  tree-sitter-norg = norg.defaultPackage.${final.system};
+                  tree-sitter-norg-meta = norg-meta.defaultPackage.${final.system};
+                };
+                builtGrammars = (filterAttrs (_: isDerivation) p.nvim-treesitter.builtGrammars) // norgGrammars;
+                allGrammars = attrValues builtGrammars;
+                withPlugins = grammarFn:
+                  p.nvim-treesitter.withPlugins (_:
+                    let plugins = (grammarFn builtGrammars); in
+                    plugins ++ (filter (p: !(elem p plugins)) (attrValues norgGrammars))
+                  );
+                withAllGrammars = withPlugins (_: allGrammars);
+              in
+              p.nvim-treesitter.overrideAttrs (a:
+                {
+                  passthru = {
+                    inherit builtGrammars allGrammars withPlugins withAllGrammars;
+                  };
+                });
+            neorg = final.vimUtils.buildVimPluginFrom2Nix {
+              pname = "neorg";
+              version = neorg.rev;
+              src = neorg;
+              dependencies = [ f.plenary-nvim (f.nvim-treesitter.withPlugins (_: [ ])) ];
+            };
+            neorg-telescope = final.vimUtils.buildVimPluginFrom2Nix {
+              pname = "neorg-telescope";
+              version = neorg-telescope.rev;
+              src = neorg-telescope;
+              dependencies = [ f.telescope-nvim f.neorg ];
+            };
+          });
         };
-      in
-      {
-        vimPlugins = prev.vimPlugins // {
-          neorg = prev.vimUtils.buildVimPluginFrom2Nix {
-            pname = "neorg";
-            version = neorg.rev;
-            src = neorg;
-            buildInputs = [ prev.vimPlugins.plenary-nvim ];
-          };
-          neorg-telescope = prev.vimUtils.buildVimPluginFrom2Nix {
-            pname = "neorg-telescope";
-            version = neorg-telescope.rev;
-            src = neorg-telescope;
-            buildInputs = [ prev.vimPlugins.telescope-nvim final.vimPlugins.neorg ];
-          };
-        };
-        tree-sitter = prev.tree-sitter // {
-          allGrammars = (prev.lib.lists.remove prev.tree-sitter.builtGrammars.tree-sitter-norg prev.tree-sitter.allGrammars) ++ builtins.attrValues grammars;
-          builtGrammars = prev.tree-sitter.builtGrammars // grammars;
-        };
-        tree-sitter-grammars = prev.tree-sitter-grammars // grammars;
-      };
-  };
+    } // (flake-utils.lib.eachDefaultSystem (system: {
+      checks = import ./tests.nix
+        (import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        });
+    }));
 }
